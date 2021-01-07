ed9069d287264f1e00fef1c443740691812da9ad
irinazhuravleva <irinazhuravleva@yandex.ru>
2021-01-07 17:58:42 +0300

    Additions

--- cypress/integration/examples/my-test.spec.js
+++ cypress/integration/examples/my-test.spec.js
@@ -6,10 +6,12 @@ context('Misc', () => {
     })
 
     it('check sequence of cards forward', () => {
+
+        for (var i = 1; i < 47; i ++ ){
         
         cy.cardCycle(1);
-        cy.cardCycle(2);
-        lastCard();
+}
+        cy.lastCard();
 
         //потом надо нажать и увидеть текст либо с ошибками, либо со Спасибо,
         // т.е. проверить, что у app  исчез дочерний элемент #card
